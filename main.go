4419f5f04b3d2d3f409332e287936e4456e4589b
Nicholas Barretta <nbarretta@nbarretta-macbookair.roam.corp.google.com>
2017-04-18 14:43:12 -0400

    Updated message again

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello Nicholas!")
+    io.WriteString(w, "Hello Nicholas!  I'm your updated app!")
 }
 
 func main() {
